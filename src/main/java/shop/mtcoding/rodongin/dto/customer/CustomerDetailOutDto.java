32af3af0d916579519ce31e2b7b53a0201f92d2d
commulcourse <commulcourse@gmail.com:git config --golbal user.email commulcourse@gmail.comgit config --global user.email commulcourse@gmail.com>
2023-03-23 13:47:33 +0900

    고객센터 게시글 보기 완료

--- src/main/java/shop/mtcoding/rodongin/dto/customer/CustomerDetailOutDto.java
+++ src/main/java/shop/mtcoding/rodongin/dto/customer/CustomerDetailOutDto.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 
 import lombok.Getter;
 import lombok.Setter;
+import shop.mtcoding.rodongin.model.employee.Employee;
 
 @Setter
 @Getter // updateDto - findCustomerDetail
@@ -12,8 +13,15 @@ public class CustomerDetailOutDto {
   private Integer id;
   private String customerTitle;
   private String customerContent;
-  private Integer employeeId;
-  private String employeeName;
+  private EmployeeDto employee;
   private Timestamp createdAt;
+
+  @Setter
+  @Getter
+  public static class EmployeeDto {
+    private Integer id;
+    private String employeeName;
+
+  }
 
 }
